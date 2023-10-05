9b6c3441180d72f61e33147ef4ffc33658dfd2aa
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-10-05 14:14:03 +0600

    AddDrivers

--- src/hotel/relax/Conn.java
+++ src/hotel/relax/Conn.java
@@ -1,9 +1,17 @@
 package hotel.relax;
 
+import java.beans.Statement;
+import java.sql.Connection;
+
 public class Conn {
+    Connection c;
+    Statement s;
     Conn(){
       try{
-          Class.forName("com.mysql.jdbc.Driver");
+         Class.forName("com.mysql.jdbc.Driver");
+         c = DriverManager.getConnection("jdbc:mysql:///hotelmangementsystem","root","");
+         s = c.createStatement();
+         
       }catch(Exception e){
         e.printStackTrace();
       }
